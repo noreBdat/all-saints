e62af935c847fe19fee57f7aab60a6ca7e41ec83
noreBdat <77025695+noreBdat@users.noreply.github.com>
2021-01-11 13:32:46 +0200

    navigation

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@ $('header ul li').on('click', function () {
     }
     
   });
+
   let header = document.querySelector("header");
  let ham = document.querySelector(".ham");
  let menu = document.querySelector(".menu");
@@ -22,51 +23,16 @@ $('header ul li').on('click', function () {
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-      header.classList.remove("hide");
-    // header.style.transform = "translateY(0)";
+      // header.classList.remove("hide");
+    header.style.transform = "translateY(0)";
   } else {
-    menu.classList.remove("change");
-    ham.classList.remove("open");
-    header.classList.add("hide");
-    // header.style.transform = "translateY(100%)";
+    // menu.classList.remove("change");
+    // ham.classList.remove("open");
+    // header.classList.add("hide");
+    header.style.transform = "translateY(-100%)";
   }
   prevScrollpos = currentScrollPos;
 }
-
-
-// $('.slider').slick({
-  
-//   arrows: true,
-//   autoplay: false,
- 
-//   centerMode: true,
-//   variableWidth: true,
-//   centerPadding: '0',
-//   nextArrow: "#next",
-//   prevArrow:"#prev",
-//   responsive: [
-//     {
-//       breakpoint: 9999,
-//       settings: 'unslick'
-      
-//     },
-//     {
-//       breakpoint: 767,
-//       settings: {
-//         slidesToShow: 2,
-//         slidesToScroll: 2,
-//         infinite: true,
-//         dots: false,
-//       }
-//     },
-    
-     
-
-//     ]
-  
-// });
-
-
 
 
 $(document).ready(function() {
